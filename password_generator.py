447dd031fe42cae8791032f7e9ab6c2dd631dc06
harounathiam2005 <92055625+harounathiam2005@users.noreply.github.com>
2022-08-03 12:22:09 -0400

    Update password_generator.py

--- password_generator.py
+++ password_generator.py
@@ -4,10 +4,10 @@ import random
 from random import shuffle
 
 # string of numbers, letters, and characters from which certain characters will be pulled.
-caps = "A B C D E F G H I J K L M N O P Q R S T U V W X Y Z"
-lowr = "a b c d e f g h i j k l m n o p q r s t u v w x y z"
-nums = "1 2 3 4 5 6 7 8 9 0"
-symb = "! @ # $ % &"
+caps = "A B C D E F G H I J K L M N O P Q R S T U V W X Y Z" # Max 26
+lowr = "a b c d e f g h i j k l m n o p q r s t u v w x y z" # Max 26
+nums = "1 2 3 4 5 6 7 8 9 0" # Max 10
+symb = "! @ # $ % &" # Max 6
 
 # Characters in string are split into individual items; string converted to list.
 capsstr = caps.split()
