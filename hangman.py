37ba9e3c35f22f08a20861f77d74d2efee9a2ba6
Harouna Thiam <92055625+harounathiam2005@users.noreply.github.com>
2022-08-05 18:42:52 -0400

    Update hangman.py

--- hangman.py
+++ hangman.py
@@ -2,7 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 import random
 
-# Pseudocode for word retrieval system located in webcrawler.py file
+# Pseudocode for word retrieval system located in Webcrawler.py
 url = "https://greenopolis.com/list-of-nouns/"
 res = requests.get(url)
 content = res.text
@@ -20,25 +20,40 @@ for i in spltext:
     if "/" in i:
         spltext.remove(i)
 
-# Using random module, select a word randomly from "spltext"
-print("Items:", len(spltext))
-print(spltext)
-select = random.randrange(1, len(spltext))
-print("From", len(spltext), "items...")
-print("Item", select, "selected")
+# Player select settings
+mode = input(print("Select difficulty: Enter '1' for easy mode; Enter '2' for hard mode."))
+print(mode)
+
+# Using module 'random', select a random word from "spltext" depending on mode chosen
+if int(mode) == 1:
+    print("Items:", len(spltext))
+    print(spltext)
+    select = random.randrange(1, 97)
+    print("From", 97, "items...")
+    print("Item", select, "selected")
+elif int(mode) == 2:
+    print("Items:", len(spltext))
+    print(spltext)
+    select = random.randrange(98, len(spltext))
+    print("From", len(spltext), "items...")
+    print("Item", select, "selected")
+else:
+    print("Please enter '1' or '2'.")
 
 # Place selected word into a variable and capitalize
+# noinspection PyUnboundLocalVariable
 lword = spltext[select]
 str(lword)
 word = lword.upper()
 
-# Get number of letters using len(), make a list of empty spaces representing each letter.
+# Get number of letters, make a list of empty spaces representing each letter.
 print(len(word), "characters")
 blanks = list('_' * len(word))
 print((" ".join(blanks)))
 
 
-def stickman():
+# Victory visual
+def stickmanv():
     print("_\|/^")
     print(" (_oo /")
     print("/-|--/")
@@ -48,19 +63,64 @@ def stickman():
     print(" L")
 
 
+lives = 6
+guessblanks = []
+
+
+# Stickman life visual
+def stickman():
+    if lives == 6:
+        print("   O   ")
+        print("-- | --")
+        print("   |   ")
+        print("  / \  ")
+    elif lives == 5:
+        print("   O   ")
+        print("-- |   ")
+        print("   |   ")
+        print("  / \  ")
+    elif lives == 4:
+        print("   O   ")
+        print("   |   ")
+        print("   |   ")
+        print("  / \  ")
+    elif lives == 3:
+        print("   O   ")
+        print("   |   ")
+        print("   |   ")
+        print("  /    ")
+    elif lives == 2:
+        print("   O   ")
+        print("   |   ")
+        print("   |   ")
+        print("       ")
+    elif lives == 1:
+        print("   O   ")
+        print("       ")
+        print("       ")
+        print("       ")
+
+
+stickman()
+
+# Game start
 while True:
 
     # Check if word is complete. If yes, end game.
     if '_' not in blanks:
         print("Congratulations!")
+        stickmanv()
         stickman()
+        print(word)
+        break
+    elif lives == 0:
+        print("You lose!")
         print(word)
         break
     else:
 
         # Get player input, place input inside variable.
-        lguess = input("Take a guess!")
-        guess = lguess.upper()
+        guess = input("Take a guess!").upper()
 
         # If guess in part of selected word...
         if guess in word:
@@ -68,6 +128,13 @@ while True:
             # Check for errors.
             if guess in blanks:
                 print("You already guessed that!")
+                lives = lives - 1
+                print("Lives: ", lives)
+                if guess not in guessblanks:
+                    guessblanks.append(guess)
+                    print(guessblanks)
+                stickman()
+
             elif guess == "":
                 print("Please enter a letter.")
             elif len(guess) != 1:
@@ -77,17 +144,30 @@ while True:
             else:
                 print("Letter correct!", guess)
                 wordlist = list(word)
-                # print(wordlist)
                 for i, j in enumerate(word):
                     if j == guess:
                         blanks[i] = j
             print(" ".join(blanks))
+            print(guessblanks)
+            stickman()
             print("")
 
         # If guess is not in word, restart loop.
         elif guess not in word:
+
+            # Error check
             if len(guess) != 1:
                 print("Guess one letter.")
             print("Incorrect!", guess)
+            if guess not in guessblanks:
+                guessblanks.append(guess)
+                print(guessblanks)
+            else:
+                print("Already guessed!")
+                print(guessblanks)
             print(' '.join(blanks))
+            lives = lives - 1
+            print("Lives: ", lives)
+            stickman()
             print("")
+            
